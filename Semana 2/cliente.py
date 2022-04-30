cbd90c62882b2ce3ae48e1b8e0650075b792fb98
Thalles Nonato <eusouthalles@gmail.com>
2022-04-30 20:56:29 -0300

    Lab 2

--- Semana 2/cliente.py
+++ Semana 2/cliente.py
@@ -1,16 +1,26 @@
 import socket
 
+#definindo host e porta
 HOST = 'localhost'
 PORTA = 5010
 
+#criar o descritor do socket
 sock = socket.socket();
+
+#estabelecer conexao
 sock.connect((HOST,PORTA))
 
+#enviar msg de hello
 texto = input();
 
+#loop só acaba quando o usuário da enter
 while texto:
+    #envia o nome do arquivo.txt
     sock.send(bytes(texto,'utf-8'))
+    #recebe a resposta do servidor
     msg = sock.recv(1024);
     print(str(msg,encoding='utf-8'))
     texto = input();
+
+#encerrar a conexão
 sock.close();
--- Semana 2/servidor.py
+++ Semana 2/servidor.py
@@ -1,6 +1,8 @@
 import socket
+#biblioteca para pegar um contador
 from collections import Counter
 
+#algoritmo para contar palavras
 def contadorPalavras(texto):
     count = Counter();
     palavras = texto.split()
@@ -8,9 +10,11 @@ def contadorPalavras(texto):
     for palavra in palavras:
         if palavra not in frequencia_palavras:
             count[palavra]+=1
+    #retorna as 5 palavras mais comuns
     return count.most_common(5)
 
 def main():
+    #definindo host, porta e socket
     HOST = ''
     PORTA = 5010
     sock = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
@@ -19,12 +23,13 @@ def main():
     novoSock , endereco = sock.accept()
     print('Conectado com: ' + str(endereco))
     while True:
+        #recebe o arquivo que vai ser lido
         msg = novoSock.recv(1024) 
         if not msg: break
         with open(str(msg,encoding='utf-8')) as file:
             texto = file.read().lower()
+            #envia as strings em ordem (.send da biblioteca socket não aceita tuple,list e etc.. aparentemente só string)
             novoSock.send(bytes(str(contadorPalavras(texto)),'utf-8'))
-    print('a')
     novoSock.close();
     sock.close(); 
 main()
