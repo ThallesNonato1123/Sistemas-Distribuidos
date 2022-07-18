1d081599ec1c42c94e42a549cb294b53534c5feb
Thalles Nonato <eusouthalles@gmail.com>
2022-07-18 01:16:16 -0300

    Trab 2, quase finalizado.Falta redistribuir nós e alterar string para arquivo

---
+++ Trabalho2/teste.py
@@ -0,0 +1,13 @@
+test_dict = {
+    0:{'host': 'localhost', 'port':10000},
+    1:{'host': 'localhost', 'port':10001},
+    2:{'host': 'localhost', 'port':10002},
+    3:{'host': 'localhost', 'port':10003},
+    4:{'host': 'localhost', 'port':10004},
+    5:{'host': 'localhost', 'port':10005},
+    6:{'host': 'localhost', 'port':10006},
+    7:{'host': 'localhost', 'port':10007}
+    }#
+
+
+print(list(test_dict).index(4))
